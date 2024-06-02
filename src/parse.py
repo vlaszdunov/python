1eada3876a3125ac9459a8faf47fb93abc397c73
vlaszdunov <design@formo.design>
2024-06-02 05:29:59 +0300

    get actuail stable releases from python.org

--- src/parse.py
+++ src/parse.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup as bs
+import re
 
 URL = 'https://www.python.org/downloads/'
 
@@ -21,8 +22,18 @@ page_data = bs(response, 'lxml')
 stable_releases_raw_links = page_data.find_all('a')
 
 stable_releases_links = []
-
-
 for link in stable_releases_raw_links:
-    if link.text == 'Gzipped source tarball':
+    if '.tar.xz' not in link.get('href') and '.tgz' in link.get('href'):
         stable_releases_links.append(link.get('href'))
+
+actual_stable_releases = []
+stable_releases_links = re.findall(
+    r'.*/\d*.\d*.?\d*/Python-\d*.\d*.?\d*.tgz', str(stable_releases_links))[0].replace('\'', '').split(', ')
+stable_releases_links[0] = stable_releases_links[0][1:]
+
+for link in stable_releases_links:
+    if re.sub(r'.?\d*/Python.*', '', link)[34:] in actual_releases_list:
+        actual_stable_releases.append(link)
+# print(*stable_releases_links, sep='\n')
+print(actual_stable_releases)
+print(actual_releases_list)
