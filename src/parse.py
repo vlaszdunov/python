91206fb54ac4b099bdadb50d80562c7c36526777
Vlas <72079892+vlaszdunov@users.noreply.github.com>
2024-06-02 09:50:50 +0300

    get links for python stable releases (#1)
    * get links for python stable releases
    
    * get list of python actual releases. Code refactoring
    
    * get actuail stable releases from python.org
    
    * refactoring of parse.py
    
    * update parse.py
    
    * fix parse.py missprint

---
+++ src/parse.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import re
+
+URL = 'https://www.python.org/downloads/'
+
+# get list of active releases
+response = requests.get(URL).text
+
+page_data = bs(response, 'lxml')
+actual_versions_raw_list = page_data.find_all(
+    'span', attrs={'class': 'release-version'})
+
+actual_releases_list = []
+for span in actual_versions_raw_list:
+    actual_releases_list.append(span.text)
+
+
+# get all stable releases
+response = requests.get(f'{URL}source/').text
+page_data = bs(response, 'lxml')
+stable_releases_raw_links = page_data.find_all('a')
+
+releases_links = []
+for link in stable_releases_raw_links:
+    releases_links.append(link.get('href'))
+
+
+actual_stable_releases_links = []
+
+for actual_release in actual_releases_list:
+    for link in releases_links:
+        check = re.fullmatch(r'.*Python-\d*.\d*.?\d*.tgz', link)
+        if check:
+            if actual_release in re.sub(r'.?\d*/Python*', '', link):
+                actual_stable_releases_links.append(link)
+                break
