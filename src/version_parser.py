46a5303be96a23bede4234c346ed173e2f39262d
vlaszdunov <design@formo.design>
2024-06-05 16:03:21 +0300

    add function for getting stable releases links

--- src/version_parser.py
+++ src/version_parser.py
@@ -1,6 +1,6 @@
 import json
-from pathlib import Path
 import re
+from pathlib import Path
 
 import requests
 from bs4 import BeautifulSoup as bs
@@ -15,8 +15,8 @@ def get_page_content(base_url, internal_page: str = '') -> bs:
 
 
 def get_active_versions(page_content: bs) -> list[str]:
-    active_versions_raw_list = page_content.find_all(
-        'span', attrs={'class': 'release-version'})
+    active_versions_raw_list = page_content\
+        .find_all('span', attrs={'class': 'release-version'})
 
     active_versions_list = []
     for element in active_versions_raw_list:
@@ -24,3 +24,18 @@ def get_active_versions(page_content: bs) -> list[str]:
             active_versions_list.append(element.text)
 
     return active_versions_list
+
+
+def get_stable_releases_links(page_content: bs, active_versions_list: list)->list[str]:
+    active_stable_releases_link = []
+    stable_releases_links = page_content.find_all('a')
+    for link in stable_releases_links:
+        if re.fullmatch(r'.*Python-\d.\d*.?\d*.tgz', link.get('href')):
+            if re.search(r'\d+.\d+', link.get('href'))[0] in active_versions_list:
+                active_stable_releases_link.append(link.get('href'))
+
+    return active_stable_releases_link
+
+
+def get_newest_releases_links(active_stable_releases_link: list[str]) -> list[str]:
+    pass
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -5,9 +5,13 @@ import pytest
 from src.version_parser import *
 
 BASE_URL = 'https://www.python.org/'
+correct_test_download_links = ['https://www.python.org/ftp/python/3.11.5/Python-3.11.4.tgz',\
+                              'https://www.python.org/ftp/python/3.11.5/Python-3.11.9.tgz']
 
-
-test_html = bs(Path('tests/test_html.html').read_text(), 'lxml')
+test_active_version_page = bs(Path('tests/test_active_versions.html')
+                              .read_text(), 'lxml')
+test_download_links_page = bs(Path('tests/test_download_link.html')
+                              .read_text(), 'lxml')
 
 
 def test_get_page_content():
@@ -15,6 +19,16 @@ def test_get_page_content():
     assert len(page_content.contents) != 0
 
 
-def test_get_versions():
-    actual_versions = get_active_versions(test_html)
+def test_get_active_versions():
+    actual_versions = get_active_versions(test_active_version_page)
     assert actual_versions == ['3.11']
+
+
+def test_get_stable_releases_link():
+    active_versions_links = get_stable_releases_links(
+        test_download_links_page, ['3.11'])
+    assert active_versions_links == correct_test_download_link
+
+def test_get_newest_releases_links():
+    latest_releases_links = get_newest_releases_links(correct_test_download_links)
+    assert latest_releases_links == ['https://www.python.org/ftp/python/3.11.5/Python-3.11.9.tgz']
